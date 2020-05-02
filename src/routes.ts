f74c86e88e907c5705e92661e68ae0227336f08b
Lorenalgm <lorena@proesc.com>
2020-05-02 15:47:12 -0300

    Tipados objetos e vetores

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,15 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response){
+    const user = createUser({
+        email: 'lorena@proesc.com',
+        password: '123456',
+        techs: [ 
+            'Node',
+            { title: 'React', experience: 100 }
+        ],
+    });
+
     return response.json({ message: 'Hello World '});
 }
